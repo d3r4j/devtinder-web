a7d43007b8e2d72d0c7c422d9bb4d6f1a91a9590
dhiraj <dhirajc1337@gmail.com>
2025-06-17 12:31:43 +0530

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    ProfileComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
